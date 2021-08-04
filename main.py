ac760b44f60baf750ccaaa9e5eba41a5071aa252
chim <zatout.chayma@gmail.com>
2021-08-04 21:45:17 +0100

    Added inference.

--- main.py
+++ main.py
@@ -1,68 +1,87 @@
-# MIT License
-# Copyright (c) 2019 JetsonHacks
-# See license
-# Using a CSI camera (such as the Raspberry Pi Version 2) connected to a
-# NVIDIA Jetson Nano Developer Kit using OpenCV
-# Drivers for the camera and OpenCV are included in the base image
-
 import cv2
+import os
+import time
+import argparse
 
-# gstreamer_pipeline returns a GStreamer pipeline for capturing from the CSI camera
-# Defaults to 1280x720 @ 60fps
-# Flip the image by setting the flip_method (most common values: 0 and 2)
-# display_width and display_height determine the size of the window on the screen
+import pycuda.autoinit  # This is needed for initializing CUDA driver
+
+from Yolo_TensorRT.utils.yolo_classes import get_cls_dict
+from Yolo_TensorRT.utils.camera import add_camera_args, Camera
+from Yolo_TensorRT.utils.display import open_window, set_display, show_fps
+from Yolo_TensorRT.utils.visualization import BBoxVisualization
+from Yolo_TensorRT.utils.yolo_with_plugins import TrtYOLO
 
 
 def gstreamer_pipeline(
-    capture_width=640,
-    capture_height=480,
-    display_width=640,
-    display_height=480,
-    framerate=60,
-    flip_method=0,
+        capture_width=640,
+        capture_height=480,
+        display_width=640,
+        display_height=480,
+        framerate=60,
+        flip_method=0,
 ):
     return (
-        "nvarguscamerasrc ! "
-        "video/x-raw(memory:NVMM), "
-        "width=(int)%d, height=(int)%d, "
-        "format=(string)NV12, framerate=(fraction)%d/1 ! "
-        "nvvidconv flip-method=%d ! "
-        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
-        "videoconvert ! "
-        "video/x-raw, format=(string)BGR ! appsink"
-        % (
-            capture_width,
-            capture_height,
-            framerate,
-            flip_method,
-            display_width,
-            display_height,
-        )
+            "nvarguscamerasrc ! "
+            "video/x-raw(memory:NVMM), "
+            "width=(int)%d, height=(int)%d, "
+            "format=(string)NV12, framerate=(fraction)%d/1 ! "
+            "nvvidconv flip-method=%d ! "
+            "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+            "videoconvert ! "
+            "video/x-raw, format=(string)BGR ! appsink"
+            % (
+                capture_width,
+                capture_height,
+                framerate,
+                flip_method,
+                display_width,
+                display_height,
+            )
     )
 
+
 if __name__ == "__main__":
-   # To flip the image, modify the flip_method parameter (0 and 2 are the most common)
+    # YOLO conf:
+    print('Yolo initialization ...')
+    model = "yolov4-416"  # in yolo dir
+    category_num = 80
+    letter_box = False
+    start = time.time()
+    trt_yolo = TrtYOLO(model, category_num, letter_box)
+    print('__time__' + str(time.time() - start) + ' s.')
+    conf_th = 0.3
+    cls_dict = get_cls_dict(category_num)
+
+    # CAMERA:
     print(gstreamer_pipeline(flip_method=0))
     cap = cv2.VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
+
     if cap.isOpened():
-        h, w= 480, 640
-        window_handle = cv2.namedWindow("Input", cv2.WINDOW_AUTOSIZE)
+        h, w = 480, 640
+        window_handle = cv2.namedWindow("Yolo", cv2.WINDOW_AUTOSIZE)
         # Window
-        while cv2.getWindowProperty("Input", 0) >= 0:
+        while cv2.getWindowProperty("Yolo", 0) >= 0:
             # Read data:
             ret_val, img = cap.read()
             # resize:
             y = (w - h) // 2
             img = img[:, y:y + h]
             img = cv2.resize(img, (416, 416))
-            cv2.imshow("Input", img)
-            
+
             # Inference:
+            start = time.time()
+            boxes, confs, clss = trt_yolo.detect(img, conf_th)
+            print('__time__' + str(time.time() - start) + ' s.')
 
             # Get classes:
 
+            # Visualize the detection:
+            vis = BBoxVisualization(cls_dict)
+            img = vis.draw_bboxes(img, boxes, confs, clss)
+            cv2.imshow("Yolo", img)
+
             # Send to server:
-            
+
             # Stop the program on the ESC key
             keyCode = cv2.waitKey(1) & 0xFF
             if keyCode == 27:
