d07ae8dd8c6afbc91efbc43353c1446051c3edea
chim <zatout.chayma@gmail.com>
2021-08-05 18:52:11 +0100

    Distance added.

--- main.py
+++ main.py
@@ -43,11 +43,28 @@ def gstreamer_pipeline(
     )
 
 
+objects_size = {'large': 416 * 416 * 75 / 100, 'medium': 416 * 416 * 50 / 100,
+                'small': 416 * 416 * 25 / 100}
+
+
+def compute_distance(bb, cls):
+    x_min, y_min, x_max, y_max = bb[0], bb[1], bb[2], bb[3]
+    s = abs(x_max - x_min) * abs(y_max - y_min)
+    if s >= objects_size[cls]*75/100:
+        return 3
+    elif s >= objects_size[cls]*25/100:
+        return 2
+    else:
+        return 1
+
 if __name__ == "__main__":
     # classes :
     print('Client initialization ...')
     semantic_classes = {"bench": 2, "chair": 2, "sofa": 2, "bed": 2,
-                     "diningtable": 3, "sink": 3, "toilet": 4}
+                        "diningtable": 3, "sink": 3, "toilet": 4}
+    distance_classes = {"bench": 'small', "chair": 'small', "sofa": "medium",
+                        "bed": "large",
+                        "diningtable": "medium", "sink": "medium", "toilet": 'small'}
     previous_class = -1
     client = Client()
 
@@ -91,20 +108,24 @@ if __name__ == "__main__":
 
             # Get classes:
             semantic_class = 0
+            distance_class = 0
             if len(confs) > 0:
                 max_p = np.argmax(confs)
                 cl = int(clss[max_p])
                 cls_name = cls_dict.get(cl, 'CLS{}'.format(cl))
-                print(cls_name+" : " + str(confs[max_p]))
+                print(cls_name + " : " + str(confs[max_p]))
                 if cls_name in semantic_classes.keys():
                     semantic_class = semantic_classes[cls_name]
+                    distance_class = compute_distance(boxes[max_p],
+                                                      distance_classes[cls_name])
                 else:
                     semantic_class = 1
+                    distance_class = compute_distance(boxes[max_p], 'large')
 
             # Send to server:
             if semantic_class != previous_class:
                 previous_class = semantic_class
-                data = semantic_class, 2
+                data = semantic_class, distance_class
                 client.send_data(*data)
 
             # Stop the program on the ESC key
